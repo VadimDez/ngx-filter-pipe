89d20afd46bfaf3299b1855e729e8accc7096346
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-07-15 19:21:35 +0200

    define base path in js

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { Ng2FilterPipeAppComponent, environment } from './app/';
+import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
+import {APP_BASE_HREF} from '@angular/common';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(Ng2FilterPipeAppComponent);
+bootstrap(Ng2FilterPipeAppComponent, [
+  { provide: APP_BASE_HREF, useValue: '/' }
+]);
 
