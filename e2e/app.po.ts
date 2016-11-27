547c4214dec6447f4e991220bf0b2744bfb377c6
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-11-27 15:46:30 +0100

    removed unneeded

--- e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class Ng2FilterPipePage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('ng2-filter-pipe-app h1')).getText();
-  }
-}
--- src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './environment';
-export * from './ng2-filter-pipe.component';
