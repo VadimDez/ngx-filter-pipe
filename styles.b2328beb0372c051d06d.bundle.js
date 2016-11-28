31fdded4e1b1d35df0f9a4603c068c9a77789c81
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-11-28 21:30:28 +0100

    0.1.0

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{330:function(n,o){},601:function(n,o,c){n.exports=c(330)}},[601]);
+webpackJsonp([1,2],{331:function(n,o){},603:function(n,o,c){n.exports=c(331)}},[603]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
