05f4565870434af266c6c8978c838bbb2293e3e3
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-10-22 14:33:44 +0200

    generate minified umd

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,33 @@
 /**
  * Created by Vadym Yatsyuk on 22.06.17
  */
+import uglify from 'rollup-plugin-uglify';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+let outputFileName = 'ngx-filter-pipe.umd.js';
+const plugins = [
+  nodeResolve({
+    jsnext: true,  module: true,  extensions: ['.js']
+  })
+];
+
+if (process.env.MINIFY === 'true') {
+  outputFileName = 'ngx-filter-pipe.min.umd.js';
+  plugins.push(uglify());
+}
+
 export default {
-  format: 'umd',
-  moduleName: 'ngx-filter-pipe',
+  input: './dist/index.js',
+  output: {
+    file: `./dist/bundles/${ outputFileName }`,
+    format: 'umd',
+  },
+  name: 'ngx-filter-pipe',
+  exports: 'named',
   external: [
     '@angular/core'
   ],
+  plugins: plugins,
   onwarn: ( warning ) => {
     const skip_codes = [
       'THIS_IS_UNDEFINED',
