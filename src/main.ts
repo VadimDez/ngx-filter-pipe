1114c2efbbe92f3f55e10654d73448599676489d
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2024-05-16 22:12:20 +0200

    Migrate to Angular 17

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { appConfig } from './app/app.config';
+// import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import './../node_modules/material-design-lite';
 
-if (environment.production) {
-  enableProdMode();
-}
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch(err => console.error(err)); 
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
